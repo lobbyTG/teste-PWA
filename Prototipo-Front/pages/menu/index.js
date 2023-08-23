e8e0631112a279fbabf0bb5461be17a7aef08cd4
lobbyTG <141638833+lobbyTG@users.noreply.github.com>
2023-08-23 08:10:51 -0300

    teste PWA

---
+++ Prototipo-Front/pages/menu/index.js
@@ -0,0 +1,10 @@
+
+
+const openMenu = () =>{
+    document.getElementById("myNav").style.width = '100%';
+}
+
+const closeMenu = () =>{
+    document.getElementById("myNav").style.width = '0%';
+
+}
