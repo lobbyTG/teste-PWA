5f4ebd3918637c8acde25561a68b38f98dbf2b87
rick-silva <ricksant.1@hotmail.com>
2023-08-23 10:29:54 -0300

    up

--- pages/cadastro/index.js
+++ pages/cadastro/index.js
@@ -1,22 +1,8 @@
-// import IMask from 'imask';
-
-// // let element = document.getElementById('phonemask');
-
-// // const mask = () =>{
-// //     element.replaceChild('(00) 00000-0000')
-// // }
-
-
-// const element = document.getElementById('phonemask');
-// const maskOptions = {
-//   mask: '+{7}(000)000-00-00'
-// };
-// const mask = IMask(element, maskOptions);
 
 
 $('#phonemask').mask('(00) 00000-0000')
 
 const cadastro = () =>{
-    location.href = "../menu/index.html";
+    location.href = "/pages/menu/index.html";
     alert("Cadastro realizado")
 }
