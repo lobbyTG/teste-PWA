ae9f7bd29dd011aad6e8aa0cff687936cf8c3707
rick-silva <ricksant.1@hotmail.com>
2023-08-24 09:32:33 -0300

    test modal

--- pages/login/index.js
+++ pages/login/index.js
@@ -1,6 +1,25 @@
 
 
+
+
+
+const closeModal = document.querySelector(".closeModal")
+const modal = document.querySelector("dialog")
+
+closeModal.onclick = function(){
+    modal.close()
+}
+
+const loadModal = () =>{
+    modal.showModal()
+}
+
+
+
 const teste = () =>{
     location.href = "./pages/menu/index.html";
     alert(`Login feito com sucesso!!!`);
 }
+
+
+
